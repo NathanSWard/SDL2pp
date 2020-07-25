8b3cc26e53f69479d177c3a7a5f6e0fca9221f41
Nathan Ward <nathansward@comcast.net>
2020-07-24 23:05:40 -0600

    add general include file

---
+++ include/sdl2pp/sdl2pp.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "color.hpp"
+#include "enums.hpp"
+#include "event.hpp"
+#include "init.hpp"
+#include "message_box.hpp"
+#include "pixel.hpp"
+#include "renderer.hpp"
+#include "shapes.hpp"
+#include "surface.hpp"
+#include "texture.hpp"
+#include "util.h"
+#include "window.hpp"
